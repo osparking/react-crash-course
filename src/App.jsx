5be8b4adcd2458cf5648a1867c8598d6cf953a22
osparking <jbpark03@gmail.com>
2024-09-28 22:56:36 +0900

    import and use Product component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Product from './component/Product';
 
 function App() {
   return (
-    <div className='container text-danger'>리엑트 단기 과정에 입과한 것을 환영합니다.</div>
+    <div className='container text-danger'>리엑트 단기 과정에 입과한 것을 환영합니다.
+      <Product/>
+    </div>
   )
 }
 
