d58db68b0700e2529348706b4bfbedeff4446b0f
osparking <jbpark03@gmail.com>
2024-09-28 16:56:18 +0900

    use bootstrap text color class for the div

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-    <div className='container'>리엑트 단기 과정에 입과한 것을 환영합니다.</div>
+    <div className='container text-danger'>리엑트 단기 과정에 입과한 것을 환영합니다.</div>
   )
 }
 
