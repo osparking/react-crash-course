52a4e95526a4fe7d29ce8964394cbc6e9228664a
osparking <jbpark03@gmail.com>
2024-09-29 07:46:28 +0900

    add a text class to the unique div

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Product = () => {
-  return <div>상품 상세 정보 페이지이다.</div>;
+  return <div className='text-success'>상품 상세 정보 페이지이다.</div>;
 }
 
 export default Product
