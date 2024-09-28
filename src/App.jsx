c28254108b3faa06cbb17c5372f9f2402e48a1b3
osparking <jbpark03@gmail.com>
2024-09-28 15:39:45 +0900

    reduce import statements to one line

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
 function App() {
   return <div>리엑트 단기 과정에 입과한 것을 환영합니다.</div>
 }
 
 export default App
-pai
