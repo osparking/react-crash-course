8b6ac552615c847f78f947661cea3500ab77a24e
osparking <jbpark03@gmail.com>
2024-09-29 11:40:13 +0900

    list group of product with details

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -24,7 +24,14 @@ const Product = () => {
   return (
     <div className='text-success'>
       <ListGroup>
-        {}
+        {products.map((product, index) => (
+          <ListGroup.Item key={index}>
+            <h4>{product.name}</h4>
+            <p>가격: ₩{product.price}</p>
+            <p>설명: {product.description}</p>
+            <hr />
+          </ListGroup.Item>    
+        ))}
       </ListGroup>
 
     </div>);
