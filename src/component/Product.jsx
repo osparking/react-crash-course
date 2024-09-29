d12e6bc54ec045e5c06f60418b017555a78a17d4
osparking <jbpark03@gmail.com>
2024-09-29 14:18:27 +0900

    move two import statements as well

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import {ListGroup} from "react-bootstrap"
-import products from './ProductData';
 
 const Product = () => {
 
--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {ListGroup} from "react-bootstrap"
+import products from './ProductData';
 
 const ProductList = () => {
   return (
