bb591639f09ff9a41f33aed7db4244d893c780e8
osparking <jbpark03@gmail.com>
2024-09-29 16:32:28 +0900

    use react hook for selected item

--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {ListGroup} from "react-bootstrap"
 import products from './ProductData';
 
 const ProductList = () => {
+    const[selectedIndex, setSelectedIndex] = useState(-1)
   return (
     <section className='mt-5'>
       <h2 className="text-info">범이비누 옵션 목록</h2>
       <hr />
       <ListGroup>
         {products.map((product, index) => (
-          <ListGroup.Item key={index} onClick={() => console.log(product)}>
+          <ListGroup.Item key={index} 
+          
+                onClick={() => console.log(product)}>
             <h4>{product.name}</h4>
             <p>가격: ₩{product.price}</p>
             <p>설명: {product.description}</p>
